188e83eff7894fc21103ea02b1821ebaa45c15ac
ChanJLee <1355425625@qq.com>
2018-02-15 15:34:08 +0800

    add demo  code

--- app/src/main/java/com/chan/subtitleview/MainActivity.java
+++ app/src/main/java/com/chan/subtitleview/MainActivity.java
@@ -2,6 +2,9 @@ package com.chan.subtitleview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.chan.lib.SubtitleView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		final SubtitleView subtitleView = findViewById(R.id.subtitle);
+		findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				subtitleView.render("1234abcABC苟利国家生死以，岂因祸福避趋之");
+			}
+		});
 	}
 }
